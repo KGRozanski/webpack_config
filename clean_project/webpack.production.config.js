3737bec060588b1d522a930d8da5c78b58dcabb3
KGRozanski <kgrozanski@gmail.com>
2019-04-23 15:50:05 +0200

    Update webpack.production.config.js

--- clean_project/webpack.production.config.js
+++ clean_project/webpack.production.config.js
@@ -2,7 +2,7 @@ const CopyWebpackPlugin = require('copy-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const CleanWebpackPlugin = require('clean-webpack-plugin');
-var ImageminPlugin = require('imagemin-webpack-plugin').default
+const ImageminPlugin = require('imagemin-webpack-plugin').default;
 
 const path = require('path');
 const fs = require('fs');
