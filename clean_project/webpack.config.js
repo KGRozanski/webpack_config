fe76055e48e3142e983432e65605421e171024a4
KGRozanski <kgrozanski@gmail.com>
2019-05-16 18:32:15 +0200

    Update webpack.config.js

--- clean_project/webpack.config.js
+++ clean_project/webpack.config.js
@@ -99,7 +99,8 @@ module.exports = {
         use: [{
           loader: 'babel-loader',
           options: {
-            presets: ['@babel/preset-env']
+            presets: ['@babel/preset-env'],
+            retainLines: true
           }
         }]
       },
